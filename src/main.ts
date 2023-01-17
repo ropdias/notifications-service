fe8c269be240ef27ba463eb2e05a032a3e9a4970
Rodrigo P. Dias <rodrigopdias@hotmail.com>
2023-01-17 02:45:40 -0300

    comments added in main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(new ValidationPipe()); // We add this to enable auto-validation from Nest.js using the class-validator package
   await app.listen(3000);
 }
 bootstrap();
-
